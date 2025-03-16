f310a6373ac5301045a40d21680a95cf6f2d1fb9
viirret <viirretvaltteri@gmail.com>
2025-03-16 21:04:37 +0200

    Do navbar normally

--- scripts/include-navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/partials/navbar.html')
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector('header').innerHTML = html;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
-});
